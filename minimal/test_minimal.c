c10d93ed3d12ec09796cc179bae100b9431c1bb1
nghiaphung <ducnghia318@gmail.com>
2023-03-10 22:55:15 +0700

    add ceedling test project config file

---
+++ minimal/test_minimal.c
@@ -0,0 +1,22 @@
+#include "unity.h"
+
+// #include "mock_stm32f0xx_hal.h"
+#include "mock_stm32f0xx_hal_gpio.h"
+// #include "mock_stm32f0xx_hal_gpio_ex.h"
+
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
+
+void test_minimal_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement bit_manipulation");
+}
+
+void test_led_init(void)
+{
+    
+}
